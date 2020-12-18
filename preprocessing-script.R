4e1e87a1e4e133de317ee16dafba5beaa379fc1c
Alex-Bujorianu <lxbujorianu@gmail.com>
2020-12-18 13:05:22 +0100

    Added the code to process the 2nd dataset

--- preprocessing-script.R
+++ preprocessing-script.R
@@ -5,6 +5,12 @@ library(dplyr)
 
 dataset_110 <- read_excel("Datasets/time_series_test_110_preprocess_en.xlsx")
 dataset_110_copy <- dataset_110
+dataset_375 <- read_excel("Datasets/time_series_375_preprocess_en.xlsx")
+dataset_375_copy <- dataset_375
+
+#--------------
+# First dataset
+#-------------
 
 #Rename the inconvenient columns
 dataset_110 <- rename(dataset_110, lymphocytes = `(%)lymphocyte`)
@@ -23,11 +29,27 @@ for (i in 1:length(dataset_110$PATIENT_ID)) {
 }
 
 #Summarise patients by mean of the 3 biomarkers. Ignore missing data.
-dataset_summary <- dataset_110 %>%
+summary_110 <- dataset_110 %>%
   group_by(PATIENT_ID) %>%
   summarise(lymphocytes = mean(lymphocytes, na.rm = TRUE), 
             Lactate_dehydrogenase = mean(Lactate_dehydrogenase, na.rm = TRUE),
             C_protein = mean (C_protein, na.rm = TRUE)
   )
-  
-View(dataset_summary)
+
+#--------------
+# Second dataset
+#--------------
+ID = 0
+for (i in 1:length(dataset_375$PATIENT_ID)) {
+  if (is.na(dataset_375$PATIENT_ID[i])) {
+    dataset_375$PATIENT_ID[i] <- ID
+  }
+  else if (! is.na(dataset_375$PATIENT_ID[i])) {
+    ID = ID + 1
+  }
+}
+
+summary_375 <- dataset_375 %>%
+  group_by(PATIENT_ID) %>%
+  summarise_all(mean, na.rm = TRUE)
+#There are a lot of values missing for some columns, like TNF-α. TO DO: pick a solution.
